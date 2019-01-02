2bb42af7b58db27b94a860d008ae9ca37118b0c5
ulanav <ulanav@rambler.ru>
2019-01-02 13:59:49 +0300

    empty comments

--- WpfApplicationTestGit/MainWindow.xaml.cs
+++ WpfApplicationTestGit/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-// test wpf draw
+// test wpf draw empty
 namespace WpfApplicationTestGit
 {
     /// <summary>
