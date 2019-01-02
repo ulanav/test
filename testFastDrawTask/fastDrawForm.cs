a120961e01fbc06546ec9f07e61967d62837ea71
ulanav <ulanav@rambler.ru>
2019-01-02 09:17:02 +0300

    Third comments correction

--- testFastDrawTask/fastDrawForm.cs
+++ testFastDrawTask/fastDrawForm.cs
@@ -15,7 +15,7 @@ using System.Runtime.InteropServices;
 namespace testFastDraw
 {
 
-    // test receive and fast draw udp packets
+    // test receive and fast draw using udp packets
     public partial class fastDrawForm : Form
     {
         Image updateImg;
