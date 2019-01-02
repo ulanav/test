72dab55a0a5d5f74a6d37434d673e1ada4c9b81f
ulanav <ulanav>
2019-01-03 00:00:24 +0300

    Home

--- testFastDrawTask/fastDrawForm.cs
+++ testFastDrawTask/fastDrawForm.cs
@@ -15,7 +15,7 @@ using System.Runtime.InteropServices;
 namespace testFastDraw
 {
 
-    // test receive and fast draw using udp packets
+    // test receive and fast draw using udp packets from home
     public partial class fastDrawForm : Form
     {
         Image updateImg;
