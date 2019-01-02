099544169cb486db90637306b5765ee579695219
ulanav <ulanav@rambler.ru>
2019-01-02 13:56:10 +0300

    some add comments

--- WpfApplicationTestGit/MainWindow.xaml.cs
+++ WpfApplicationTestGit/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-// test 
+// test wpf draw
 namespace WpfApplicationTestGit
 {
     /// <summary>
